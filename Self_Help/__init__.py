f5c7998cc94b8b364304da2869fa8097dbc6cd45
LI Huabing <i@lihuabing.site>
2024-06-01 11:36:38 +0200

    Asynchrony-compatible

--- Self_Help/__init__.py
+++ Self_Help/__init__.py
@@ -48,18 +48,16 @@ def set_payoff(group: Group):
 class Player(BasePlayer):
     Sex = models.IntegerField(choices=[[0, '女'], [1, '男'], [99, '其他']], label='您的性别为：')
     Age = models.IntegerField(label='您的年龄为：', max=130, min=0)
-    Nation = models.StringField(label='您常居的国家为：')
     Education = models.IntegerField(choices=[[0, '初中及以下'], [1, '高中'], [2, '本科及同等学历'], [3, '硕士及同等学历'], [4, '博士及同等学历'], [99, '其他']], label='您的受教育程度为：')
     Law_Related = models.IntegerField(choices=[[0, '无关'], [1, '有关']], label='您的教育或工作经历是否同法学或法律事务有关？')
     Marriage = models.IntegerField(choices=[[0, '未婚'], [1, '已婚'], [2, '离异'], [3, '丧偶'], [99, '其他']], label='您的婚姻状况为：')
     Income = models.IntegerField(choices=[[0, '2000元以下'], [1, '2000元到4000元'], [2, '4000元到6000元'], [3, '6000元到8000元'], [4, '8000元到10000元'], [5, '10000元到12000元'], [6, '12000元到14000元'], [7, '14000元到16000元'], [8, '16000元到18000元'], [9, '18000元到20000元'], [10, '20000元以上']], label='您的月平均收入为（以人民币计）：')
     Email = models.StringField(blank=True, initial='None', label='您的电子邮箱为（非必填项）：')
-class Wait(WaitPage):
-    group_by_arrival_time = True
+    Nation = models.IntegerField(choices=[[0, '亚洲'], [1, '欧洲'], [2, '北美洲'], [3, '南美洲'], [4, '非洲'], [5, '大洋洲'], [99, '其他']], label='您的经常居住地位于：')
 class Welcome(Page):
     form_model = 'player'
-    form_fields = ['Sex', 'Age', 'Nation', 'Education', 'Law_Related', 'Marriage', 'Income']
-    timeout_seconds = 120
+    form_fields = ['Sex', 'Age', 'Nation', 'Education', 'Law_Related', 'Marriage', 'Income', 'Email']
+    timeout_seconds = 180
 class Description(Page):
     form_model = 'player'
     timeout_seconds = 300
@@ -73,7 +71,7 @@ class Plaintiff(Page):
         return player.id_in_group == 1
 class WaitForPlaintiff(WaitPage):
     title_text = '您被分配到的角色是：侵权者！'
-    body_text = '您被分配到的角色是：<b>侵权者</b>！目前被侵权者正在决策，请您稍作等待。'
+    body_text = '您被分配到的角色是：<b>侵权者</b>！目前被侵权者正在决策，请您稍作等待。如您在本页面等待时间超过5分钟，您可先行退出，实验团队将尝试通过邮件（如预留）向您汇报实验结果（如成功完成）。'
     @staticmethod
     def is_displayed(player: Player):
         group = player.group
@@ -113,10 +111,7 @@ class Defendant_3(Page):
 class WaitForAll(WaitPage):
     after_all_players_arrive = set_payoff
     title_text = '等待所有参与者完成实验'
-    body_text = '实验已经接近尾声，请您稍作等待。'
+    body_text = '实验已经接近尾声，请您稍作等待。如您在本页面等待时间超过5分钟，您可先行退出，实验团队将尝试通过邮件（如预留）向您汇报实验结果（如成功完成）。'
 class Result(Page):
     form_model = 'player'
-    form_fields = ['Email']
-class Thanks(Page):
-    form_model = 'player'
-page_sequence = [Wait, Welcome, Description, Plaintiff, WaitForPlaintiff, Defendant_1, Plaintiff_1, Defendant_2, Defendant_3, WaitForAll, Result, Thanks]
+page_sequence = [Welcome, Description, Plaintiff, WaitForPlaintiff, Defendant_1, Plaintiff_1, Defendant_2, Defendant_3, WaitForAll, Result]
--- Self_Help/tests.py
+++ Self_Help/tests.py
@@ -12,4 +12,17 @@ class PlayerBot(Bot):
             Law_Related=0,
             Marriage=0,
             Income=0,
+            Email="xyz",
         )
+        yield Description
+        if self.player.id_in_group == 1:
+            yield Plaintiff, dict(Plaintiff_Choice_1=0)
+        if self.player.id_in_group == 2 and self.group.Plaintiff_Choice_1 == 0:
+            yield Defendant_1, dict(Defendant_Choice_1=0)
+        if self.player.id_in_group == 1 and self.group.Plaintiff_Choice_1 == 0:
+            yield Plaintiff_1, dict(Plaintiff_Choice_2=0)
+        if self.player.id_in_group == 2 and self.group.Plaintiff_Choice_1 == 1:
+            yield Defendant_2, dict(Defendant_Choice_2=0)
+        if self.player.id_in_group == 2 and self.group.Plaintiff_Choice_1 == 2:
+            yield Defendant_3, dict(Defendant_Choice_3=0)
+        yield Result
